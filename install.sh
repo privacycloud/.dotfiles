ebe1c4b9bde62af485792ee13e990b64b61bcdbc
Fernando Freije <ffreijefuente@gmail.com>
2019-12-18 11:23:51 +0100

    Change curl for wget in nvm installer

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ brew bundle
 echo
 echo "Installing nvm..."
 echo
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 
 echo
 echo "Installing Oh My Zsh..."
